4062c709864141c38615a12322c8f4d95ca64d0c
Guilherme Vianna <mataveli91@gmail.com>
2023-12-18 12:41:29 -0300

    update imports

---
+++ src/app/register/page.tsx
@@ -0,0 +1,13 @@
+import RegisterHeader from "../components/RegisterHeader";
+import RegisterBox from "../components/RegisterBox";
+
+export default function Home() {
+  return (
+    <div>
+      <RegisterHeader />
+      <div className="flex justify-center items-center">
+        <RegisterBox />
+      </div>
+    </div>
+  )
+} 
