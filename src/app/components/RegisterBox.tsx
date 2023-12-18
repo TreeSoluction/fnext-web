ab5ae33eadf8048e777263412283e8c7a5239fce
Guilherme Vianna <mataveli91@gmail.com>
2023-12-18 12:41:08 -0300

    register box base component added

---
+++ src/app/components/RegisterBox.tsx
@@ -0,0 +1,10 @@
+export default function RegisterBox() {
+  return (
+    <div className="flex bg-slate-300 p-10">
+      <div className="flex-col">
+        <div className="text-2xl text-white bg-HIGH_BLUE text-center">FENEXT</div>
+        <div className="text-2xl text-white bg-HIGH_BLUE text-center">Preencha os Dados</div>
+      </div>
+    </div>
+  )
+}
