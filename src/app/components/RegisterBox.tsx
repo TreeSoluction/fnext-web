70fa9881d31fd7470d1d616682c094e5ec587182
Guilherme Vianna <mataveli91@gmail.com>
2023-12-18 18:53:28 -0300

    pre-register

--- src/app/components/RegisterBox.tsx
+++ src/app/components/RegisterBox.tsx
@@ -1,10 +1,9 @@
 export default function RegisterBox() {
   return (
-    <div className="flex bg-slate-300 p-10">
-      <div className="flex-col">
-        <div className="text-2xl text-white bg-HIGH_BLUE text-center">FENEXT</div>
-        <div className="text-2xl text-white bg-HIGH_BLUE text-center">Preencha os Dados</div>
+    <div className="rounded-md shadow-md mt-10">
+      <div className="text-2xl text-white bg-HIGH_BLUE text-center rounded-md">
+        FENEXT
       </div>
     </div>
-  )
+  );
 }
