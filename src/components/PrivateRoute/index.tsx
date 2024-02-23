9ca6f25a3ebc69091653bf6a87275c67929f6611
Lucasm4sco <lmbp202@gmail.com>
2024-02-23 02:17:50 -0300

    feat: created component to check if user is authenticated and deprive route

---
+++ src/components/PrivateRoute/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { APP_ROUTES } from '@/constants/app-route';
+import { checkUserIsAuthenticated } from '@/functions/check-user-is-authenticated';
+import { useRouter } from 'next/navigation';
+
+type PrivateRouteProps = {
+    children: React.ReactNode;
+}
+
+/**
+ * A component that renders its children only if the user is authenticated.
+ *
+ * @component
+ * @param {React.ReactNode} children - The children to be rendered if the user is authenticated.
+ * @returns {React.ReactNode} - The rendered children if the user is authenticated, otherwise null.
+ */
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+    const router = useRouter();
+    const isUserAuthenticated = checkUserIsAuthenticated();
+
+    React.useEffect(() => {
+        if (!isUserAuthenticated) {
+            router.push(APP_ROUTES.public.login);
+        }
+    }, [isUserAuthenticated, router]);
+
+    return (
+        <>
+            {isUserAuthenticated && children}
+        </>
+    );
+};
+
+export default PrivateRoute;
---
+++ src/functions/check-user-is-authenticated/index.ts
@@ -0,0 +1,10 @@
+import Cookies from "js-cookie";
+
+/**
+ * Checks if the user is authenticated.
+ * @returns {boolean} Returns true if the user is authenticated, false otherwise.
+ */
+export function checkUserIsAuthenticated(): boolean {
+  const token = Cookies.get("token");
+  return !!token;
+}
