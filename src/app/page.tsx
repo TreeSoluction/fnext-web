5f9aeed32428ac27698a75e4470d5743d3c60fa8
Lucasm4sco <lmbp202@gmail.com>
2024-02-24 23:05:37 -0300

    feat: applied menu bar design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import Header from "@/components/Header";
+import MenuBar from "@/components/Header/menu";
+import { APP_ROUTES } from "@/constants/app-route";
 
 export default function Home() {
     return (
         <div>
             <Header>
-                <p>Children</p>
-            </Header> 
+                <div className="flex flex-wrap-reverse justify-center items-center gap-4">
+                    <a
+                        className="text-sm text-white font-bold capitalize underline text-center"
+                        href={APP_ROUTES.private.franchises}
+                    >
+                        Cadastrar minha franquia
+                    </a>
+                    <MenuBar />
+                </div>
+            </Header>
         </div>
     );
 }
