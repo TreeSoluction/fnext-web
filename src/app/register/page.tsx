28094f53c8359d756609a5fdbda531bb8b6448a3
AlehandroSV <alehandrosantos99@gmail.com>
2024-04-23 19:08:16 -0300

    fix: ajustando estrutura de Fetches

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,14 +4,14 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import api from "../services/api";
+import api from "../../services/api";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import {
   notifyInfo,
   notifySuccess,
   notifyError,
-} from "../services/notification";
+} from "../../services/notification";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 import ErrorMessage from "@/components/Form/error-message";
@@ -20,17 +20,18 @@ import { InputDate, InputStyled, InputMasked } from "@/components/Form/inputs";
 export default function Home() {
   const router = useRouter();
   const RegisterSchema = z.object({
-    fullname: z.string().min(1, 'Nome não pode ser vazio!'),
+    fullname: z.string().min(1, "Nome não pode ser vazio!"),
     email: z.string().email({ message: "Email Inválido!" }),
     password: z.string().min(6, "A senha tem que ter no mínimo 6 caracteres!"),
-    date: z.string()
-      .regex(/^\d{4}-\d{2}-\d{2}$/, 'Data Inválida!'),
-    phone: z.string()
-      .regex(/^\(\d{2}\) \d{5}-\d{4}$/, 'Telefone Inválido!')
-      .transform((phone) => phone.replace(/[()\-]/g, '')),
-    cnpj: z.string()
-      .regex(/^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/, 'CNPJ Inválido!')
-      .transform((cnpj) => cnpj.replace(/[\.\/\-]/g, ''))
+    date: z.string().regex(/^\d{4}-\d{2}-\d{2}$/, "Data Inválida!"),
+    phone: z
+      .string()
+      .regex(/^\(\d{2}\) \d{5}-\d{4}$/, "Telefone Inválido!")
+      .transform((phone) => phone.replace(/[()\-]/g, "")),
+    cnpj: z
+      .string()
+      .regex(/^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/, "CNPJ Inválido!")
+      .transform((cnpj) => cnpj.replace(/[\.\/\-]/g, "")),
   });
 
   const {
@@ -38,7 +39,7 @@ export default function Home() {
     handleSubmit,
     formState: { errors },
     reset,
-    setValue
+    setValue,
   } = useForm({
     resolver: zodResolver(RegisterSchema),
   });
@@ -57,7 +58,7 @@ export default function Home() {
         password: data.password,
         birth_date: data.date,
         phone: data.phone,
-        cnpj: data.cnpj
+        cnpj: data.cnpj,
       })
       .then((res) => {
         if (res.status === 201) {
@@ -70,10 +71,12 @@ export default function Home() {
       .catch((err) => {
         if (err.response.status < 500) {
           notifyError(err.response.data.message);
-          return
+          return;
         }
 
-        notifyError("Ocorreu um erro inesperado ao servidor, tente novamente mais tarde!");
+        notifyError(
+          "Ocorreu um erro inesperado ao servidor, tente novamente mais tarde!"
+        );
       });
   };
 
@@ -83,33 +86,23 @@ export default function Home() {
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col w-full max-w-lg p-6 sm:px-12 sm:py-6 bg-white rounded-[5px] sm:rounded-[10px]  shadow-xl"
       >
-        <h1 className="text-4xl my-2">
-          Dados Pessoais
-        </h1>
+        <h1 className="text-4xl my-2">Dados Pessoais</h1>
         <p className="mt-4 text-sm">
-          Duvidas no cadastro? <a className="text-LOW_BLUE underline font-bold" href="#">ajuda</a>
+          Duvidas no cadastro?{" "}
+          <a className="text-LOW_BLUE underline font-bold" href="#">
+            ajuda
+          </a>
         </p>
-        <InputStyled
-          label="Nome"
-          {...register("fullname")}
-        />
+        <InputStyled label="Nome" {...register("fullname")} />
         <ErrorMessage error={errors.fullname} />
-        <InputStyled
-          label="E-mail"
-          type="email"
-          {...register("email")}
-        />
+        <InputStyled label="E-mail" type="email" {...register("email")} />
         <ErrorMessage error={errors.email} />
-        <InputStyled
-          label="Senha"
-          type="password"
-          {...register("password")}
-        />
+        <InputStyled label="Senha" type="password" {...register("password")} />
         <ErrorMessage error={errors.password} />
         <InputDate
           label="Data de Nascimento"
-          getInputDateRegisterRef={() => register('date')}
-          setValueInputDate={(value: string) => setValue('date', value)}
+          getInputDateRegisterRef={() => register("date")}
+          setValueInputDate={(value: string) => setValue("date", value)}
         />
         <ErrorMessage error={errors.date} />
         <InputMasked
@@ -125,13 +118,21 @@ export default function Home() {
         />
         <ErrorMessage error={errors.cnpj} />
         <p className="max-w-80 text-xs mt-8">
-          Ao clicar em <strong>&quot;Finalizar&quot;</strong> e continuar com o seu cadastro, você está concordando com a nossa <a className="text-BLACK underline font-bold" href="#">Política de Privacidade</a>.
+          Ao clicar em <strong>&quot;Finalizar&quot;</strong> e continuar com o
+          seu cadastro, você está concordando com a nossa{" "}
+          <a className="text-BLACK underline font-bold" href="#">
+            Política de Privacidade
+          </a>
+          .
         </p>
-        <button className="mx-auto my-6 w-1/2 min-w-40  bg-LOW_BLUE text-white text-xl font-bold underline py-2 rounded-[25px]" type="submit">
+        <button
+          className="mx-auto my-6 w-1/2 min-w-40  bg-LOW_BLUE text-white text-xl font-bold underline py-2 rounded-[25px]"
+          type="submit"
+        >
           Finalizar
         </button>
       </form>
       <ToastContainer />
-    </div >
+    </div>
   );
 }
---
+++ src/services/api.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosInstance } from "axios";
+
+const request = () => {
+  // TODO: Criar lógica para resgatar token aqui
+  const token = "token";
+
+  const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+  });
+
+  if (token) {
+    api.defaults.headers["Authentication"] = `Bearer ${token}`;
+  }
+
+  return api;
+};
+
+const api: AxiosInstance = request();
+
+export default api;
