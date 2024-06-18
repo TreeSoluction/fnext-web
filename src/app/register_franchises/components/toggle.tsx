288e1976a1c1d435432fe3ca2fae1221a0422b0a
AlehandroSV <alehandrosantos99@gmail.com>
2024-06-17 21:24:18 -0300

    feat: implements new styling and adjusts StatusToggle logic

---
+++ src/app/register_franchises/components/toggle.tsx
@@ -0,0 +1,50 @@
+import { ChangeEvent, forwardRef } from "react";
+
+interface StatusToggleProps {
+  id: string;
+  checked: boolean;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const StatusToggle = forwardRef<HTMLLabelElement, StatusToggleProps>(
+  ({ id, checked, onChange }, ref) => {
+    return (
+      <div className="mt-2">
+        <label
+          ref={ref}
+          className="relative inline-block w-32 h-8"
+          htmlFor={id}
+        >
+          <input
+            type="checkbox"
+            className="sr-only peer"
+            id={id}
+            checked={checked}
+            onChange={onChange}
+          />
+          <div
+            className={`w-full h-full bg-red-500 rounded-full cursor-pointer transition-all duration-300 peer-checked:bg-blue-500 peer-checked:shadow-inner outline-none ring-2 ring-offset-2 
+            ${checked ? "ring-blue-500" : "ring-red-500"}`}
+          >
+            <span
+              className={`block w-8 h-8 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
+                checked ? "translate-x-24" : "translate-x-0"
+              }`}
+            ></span>
+          </div>
+          <span
+            className={`absolute top-0 left-0 w-full h-full flex items-center justify-center text-white uppercase text-xs transition-all duration-300 ${
+              checked ? "justify-end pr-12" : "justify-start pl-8"
+            }`}
+          >
+            {checked ? "Ativo" : "Desativo"}
+          </span>
+        </label>
+      </div>
+    );
+  }
+);
+
+StatusToggle.displayName = "StatusToggle";
+
+export { StatusToggle };
