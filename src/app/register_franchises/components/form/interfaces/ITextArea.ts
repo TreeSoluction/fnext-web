b27323ae9684fdcea68b54c4c914cd4879bd9b73
AlehandroSV <alehandrosantos99@gmail.com>
2024-06-18 10:24:33 -0300

    feat: implements new TextArea component and organize folder architecture from new components

---
+++ src/app/register_franchises/components/form/interfaces/ITextArea.ts
@@ -0,0 +1,33 @@
+import { WatchdogConfig } from "@ckeditor/ckeditor5-watchdog/src/watchdog";
+import { Editor, EditorConfig } from "@ckeditor/ckeditor5-core";
+import { type InferProps } from "prop-types";
+import { EventInfo } from "@ckeditor/ckeditor5-utils";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+
+export interface ErrorDetails {
+  phase: "initialization" | "runtime";
+  willEditorRestart?: boolean;
+}
+
+export interface Props<TEditor extends Editor>
+  extends InferProps<typeof CKEditor.propTypes> {
+  editor: {
+    create(...args: any): Promise<TEditor>;
+  };
+  config?: EditorConfig;
+  watchdogConfig?: WatchdogConfig;
+  disableWatchdog?: boolean;
+  onReady?: (editor: TEditor) => void;
+  onAfterDestroy?: (editor: TEditor) => void;
+  onError?: (error: Error, details: ErrorDetails) => void;
+  onChange?: (event: EventInfo, editor: TEditor) => void;
+  onFocus?: (event: EventInfo, editor: TEditor) => void;
+  onBlur?: (event: EventInfo, editor: TEditor) => void;
+}
+
+export interface TextAreaProps extends Props<ClassicEditor> {
+  label: string;
+  maxLenght: number;
+  characters: number;
+}
