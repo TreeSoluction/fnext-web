1743cc4260a68e9957fccc5f1f368fd928112979
AlehandroSV <alehandrosantos99@gmail.com>
2024-06-18 08:22:56 -0300

    feat: improves input styling

--- src/app/register_franchises/components/input.tsx
+++ src/app/register_franchises/components/input.tsx
@@ -3,18 +3,23 @@ import { InputHTMLAttributes, forwardRef } from "react";
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
 }
-
 const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ label, id, ...props }, ref) => {
     return (
       <div>
         <label
           htmlFor={id}
-          className="block mb-1 pl-1 text-[#666] text-[0.8rem]"
+          className="block mb-1 pl-1 text-[#666] text-[0.8rem] font-secondary"
         >
           {label}
         </label>
-        <input id={id} ref={ref} {...props} />
+
+        <input
+          id={id}
+          ref={ref}
+          {...props}
+          className="w-full p-2 border border-[#ddd] border-solid rounded"
+        />
       </div>
     );
   }
